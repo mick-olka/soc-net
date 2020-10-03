423ce57d0b402d51da80b0c16f47fe5dce3a2915
mick-olka <mickolka64@gmail.com>
2020-10-03 15:15:06 +0300

    added users state

---
+++ src/redux/usersReducer.js
@@ -0,0 +1,10 @@
+
+let initialState = {
+    users: [
+        {id: 0, name: "Mick Tret"},
+        {id: 0, name: "Natal Reign"},
+        {id: 0, name: "And Nee"},
+        {id: 0, name: "Eleez Maf"},
+        {id: 0, name: "Poster Meter"},
+    ]
+}
